4f99db9e4c6275a8234264992aebb6658a629585
Balakrishna Reddy Polu <polu.balakrishna96@gmail.com>
2019-03-22 14:27:09 +0530

    Updates

---
+++ precache-manifest.3fd58d918078c118bd3ea3dbb5198e1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c30e02548e907699a0ed63b4b8219722",
+    "url": "/tweet/static/media/profile.c30e0254.png"
+  },
+  {
+    "revision": "91b53a01e3ec453809ebe07ca94f0e56",
+    "url": "/tweet/static/media/Background.91b53a01.jpg"
+  },
+  {
+    "revision": "705bfccd5210e0d02e1b",
+    "url": "/tweet/static/js/runtime~main.8264764c.js"
+  },
+  {
+    "revision": "5342d1087b2a9583cd37",
+    "url": "/tweet/static/js/main.3c1db808.chunk.js"
+  },
+  {
+    "revision": "9145f153e8cd9697355f",
+    "url": "/tweet/static/js/2.14ab709c.chunk.js"
+  },
+  {
+    "revision": "5342d1087b2a9583cd37",
+    "url": "/tweet/static/css/main.0b13b8dc.chunk.css"
+  },
+  {
+    "revision": "9145f153e8cd9697355f",
+    "url": "/tweet/static/css/2.b20903e6.chunk.css"
+  },
+  {
+    "revision": "283abec10f700dbfe957d1bd482a0f42",
+    "url": "/tweet/index.html"
+  }
+];
--- src/configureStore/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import {comments, tweetText} from "./reducers";
-const rootReducer = combineReducers({
-    comments,
-    tweetText
-})
-
-export const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
